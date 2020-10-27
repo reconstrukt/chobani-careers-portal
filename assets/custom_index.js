0e9ff72d09e30d599abff2708fc586dc4eb3f308
Matthew Knight <matt@reconstrukt.com>
2020-10-27 19:51:14 -0400

    homepage category links

--- assets/custom_index.js
+++ assets/custom_index.js
@@ -154,4 +154,104 @@ $(document).ready(function(){
 		console.log(zigzagHeight);
 		$('.bg-wrap').css('width', zigzagHeight);
 	})
+  
+  var jobCats = [{
+    title: 'Administration',
+    url: '/go/Administration/6847550/'
+  },{
+    title: 'Cafe',
+    url: '/go/Cafe/6847650/'
+  },{
+    title: 'Communication',
+    url: '/go/Communication/6847750/'
+  },{
+    title: 'Corporate Affairs',
+    url: '/go/Corporate-Affairs/6847850/'
+  },{
+    title: 'Engineering',
+    url: '/go/Engineering/6847950/'
+  },{
+    title: 'Finance',
+    url: '/go/Finance/6848050/'
+  },{
+    title: 'Information Technology',
+    url: '/go/Information-Technology/6848150/'
+  },{
+    title: 'Innovation',
+    url: '/go/Innovation/6848250/'
+  },{
+    title: 'Legal',
+    url: '/go/Legal/6848350/'
+  },{
+    title: 'Logistics',
+    url: '/go/Logistics/6848450/'
+  },{
+    title: 'Marketing',
+    url: '/go/Marketing/6848550/'
+  },{
+    title: 'People',
+    url: '/go/People/6848650/'
+  },{
+    title: 'Planning and Product Supply',
+    url: '/go/Planning-and-Product-Supply/6848750/'
+  },{
+    title: 'Plant Operations',
+    url: '/go/Plant-Operations/6848850/'
+  },{
+    title: 'Procurement',
+    url: '/go/Procurement/6848950/'
+  },{
+    title: 'Quality',
+    url: '/go/Quality/6849050/'
+  },{
+    title: 'Research & Development',
+    url: '/go/Research-&amp;-Development/6849150/'
+  },{
+    title: 'Sales',
+    url: '/go/Sales/6849250/'
+  },{
+    title: 'Sales & Operations Planning',
+    url: '/go/Sales-&amp;-Operations-Planning/6849350/'
+  },{
+    title: 'Supply Chain',
+    url: '/go/Supply-Chain/6849450/'
+  },{
+    title: 'Transporation',
+    url: '/go/Transporation/6849550/'
+  }];
+  
+  var categoryContainer = $('.js-chobani-job-categories');
+  categoryContainer.empty();
+  
+  var html = '';
+  
+  for ( var i=0; i<jobCats.length; i++ ) {
+    
+    html += '<div class="row">';
+    
+    if( typeof jobCats[i] !== 'undefined' ) {
+      html += '<div class="col-md-4">';
+      html += '<a class="h4 color-green text-center" href="' + jobCats[i].url + '">' + jobCats[i].title + '</a>';
+      html += '</div>';
+    }
+    
+    i++;
+    if( typeof jobCats[i] !== 'undefined' ) {
+      html += '<div class="col-md-4">';
+      html += '<a class="h4 color-green text-center" href="' + jobCats[i].url + '">' + jobCats[i].title + '</a>';
+      html += '</div>';
+    }
+    
+    i++;
+    if( typeof jobCats[i] !== 'undefined' ) {
+      html += '<div class="col-md-4">';
+      html += '<a class="h4 color-green text-center" href="' + jobCats[i].url + '">' + jobCats[i].title + '</a>';
+      html += '</div>';
+    }
+    
+    html += '</div>';
+  }
+  
+  categoryContainer.html( html );
+  
 });
