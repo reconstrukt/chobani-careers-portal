f6d8d64998894cd705e92cb8705e4a822040086b
Matthew Knight <matt@reconstrukt.com>
2020-10-27 20:31:57 -0400

    formatting

--- assets/custom_global.js
+++ assets/custom_global.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	
   if (location && location.href && !location.href.includes("chobani-careers.reconstrukt.net")) {
-		$("head").find("link[rel=stylesheet]").each(function(){
+    $("head").find("link[rel=stylesheet]").each(function(){
       if ( $(this).attr('href').indexOf('chobani-careers-portal') == -1 ) {
         $(this).remove();
         console.log( 'removed css: ' + $(this).attr('href') );
-      }        
+      }
     });
-	}
+  }
   
 });
