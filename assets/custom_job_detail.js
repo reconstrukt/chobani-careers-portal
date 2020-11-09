31ef547326f86e7080f7acdb8a707bd54973ccf9
Matthew Knight <matt@reconstrukt.com>
2020-11-09 15:56:03 -0500

    profile link

--- assets/custom_job_detail.js
+++ assets/custom_job_detail.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
     return;
   }
   
-  var profileLink = $('#header .profileWidget a');  
+  var profileLinks = $('#header .profileWidget a');
+  var profileLink = $( profileLink[0] );
+  profileLink.removeAttr( 'class' );
+  profileLink.addClass( 'font-weight-light mr-5 h6' );
+  
 	$("#header").replaceWith(chobaniTheme.header);
-  $('header').find('.js-chobani-header-links').append( $( profileLink[0] ) );
+  $('header').find('.js-chobani-header-links').append( profileLink );
   
   $("#footer").replaceWith(chobaniTheme.footer);
 
